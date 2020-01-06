dccabb523e6e5a5f8963343fc140b3477b97745b
Richardas <richardas.kersis@gmail.com>
2020-01-06 17:00:47 +0200

    chat finished with working server-clients

--- appscript.js
+++ appscript.js
@@ -1,30 +1,148 @@
-// const socket = io('http://localhost');
+// var $email = $("#email"); // refers to the jQuery object representation of the dom object
+// var email_field = $("#email").get(0); // refers to the dom object itself
 
-$(function(){
-	var socket = io.connect();
-	var $messageForm = $('#messageForm');
-	var $message = $('#message');
-	var $chat = $('#chat');
-	var $messageArea = $('#messageArea');
+
+//The following go under #messages div
+const SYS_MESSAGE_PRE_CONTENT = `<div class="system_message">`; // + content + closing div
+
+const YOUR_MESSAGE_PRE_CONTENT = `
+<div class="your_message">
+	<div class="your_message_text">`;
+const YOUR_MESSAGE_POST_CONTENT = `</div>
+<div class="your_message_arrow_right"></div>
+</div>`
+
+function getSysMessageUserLeftHtml(user) {
+	return SYS_MESSAGE_PRE_CONTENT + `User<strong> ` + user + ` </strong>has left the chat</div>`;
+}
+
+function getSysMessageUserJoinedHtml(user) {
+	return SYS_MESSAGE_PRE_CONTENT + `User<strong> ` + user + ` </strong>has joined the chat</div>`;
+}
+function getUserMessageHtml(msg, publicId) {
+	let colorGradientCss;
+	for (let i = 0; i < users.length; i++) {
+		if (users[i].publicId == publicId) {
+			// console.log(publicId);
+			colorGradientCss = users[i].colorGradientCss;
+			break;
+		}
+	}
+
+	return `
+	<div class="user_message">
+	<div class="user_message_circle" style="` + colorGradientCss + `"></div>
+	<div class="user_message_arrow_left"></div>
+	<div class="user_message_text">` + msg + `</div></div>
+	`;
+}
+
+function getYourMessageHtml(msg) {
+	return YOUR_MESSAGE_PRE_CONTENT + msg + YOUR_MESSAGE_POST_CONTENT;
+}
+
+function getConnectedUserHtml(publicId, username, colorGradientCss) {
+	return `
+	<div class="user" id="` + publicId + `">
+		<div class="connected_user_circle" style="` + colorGradientCss + `"></div>
+		<div class="vertically_aligned_username">
+			<div class="user_name">` + username + `</div>
+			</div>
+		</div>
+	`;
+}
+
+var socket = io.connect();
+var users = [];
+var $connectedUsers = $('#connectedUsers');
+var $userMessageForm = $('#userMessageForm');
+var $inputBox = $('#inputBox');
+var $messages = $('#messages');
+
+// Put this in registration form
+// var your_username = 'Joshua';
+var your_username = prompt('Enter your name');
+// make safe, call color by id from circle_colors.json
+var your_color_schema = Math.floor((Math.random() * 16)); //chosen from circle_colors.json
+socket.emit('new_user',
+	{
+		"username": your_username,
+		"color_schema": your_color_schema,
+		"color_schema_deg": 130
+	}
+);
+$messages.append(getSysMessageUserJoinedHtml(your_username));
+socket.emit('get_users');
+
+$(function () {
+	// var $messageArea = $('#messageArea');
 	// var $userFormArea = $('#userFormArea');
 	// var $userForm = $('#userForm');
 	// var $username = $('#username');
-	// var $users = $('#users');
+	var $users = $('#users');
 
-	$messageForm.submit(function(e){
+	//Send message
+	//----On Enter key press
+	$inputBox.on('keypress', function (key) {
+		if (key.which == 13) {
+			$userMessageForm.submit();
+			return false;
+		}
+	});
+	//----On "Send" button press
+	$userMessageForm.submit(function (e) {
 		e.preventDefault();
-		socket.emit('send message', $message.val());
-		$message.val('');
+		let msg = $inputBox.val();
+		$inputBox.val('');
+		socket.emit('send_message', msg);
+		$messages.append(getYourMessageHtml(msg));
 		// console.log('Submitted');
 	});
 
-	socket.on('new message', function(data){
-		$chat.append('<div class="well"><strong>' + data.user + ':</strong> ' + data.msg + '</div>');
+	//Receive message
+	socket.on('new_message', function (data) {
+		console.log(data.msg + " " + data.publicId);
+		$messages.append(getUserMessageHtml(data.msg, data.publicId));
 	});
 
+	//Sys messages
+	socket.on('sys_user_joined', function (data) {
+		users.push(data);
+		console.log(users);
+		$connectedUsers.append(getConnectedUserHtml(data.publicId, data.username, data.colorGradientCss));
+		$messages.append(getSysMessageUserJoinedHtml(data.username));
+	});
+
+	socket.on('sys_user_left', function (publicId) {
+		console.log('sys_user_left: ' + publicId);
+		for (let i = 0; i < users.length; i++) {
+			if (users[i].publicId == publicId) {
+				$messages.append(getSysMessageUserLeftHtml(users[i].username));
+				$('#' + publicId.toString()).remove();
+				users.splice(i, 1);
+				// delete users[i]; //leaves an "empty" element behind and thus later give a typeerror: cannot read property publicId of 'undefined'
+				break;
+			}
+		}
+	});
+
+	socket.on('serve_users', function (userlist) {
+		// console.log('got userlist');
+		// console.log(userlist);
+		users = userlist;
+		let connectedUsersHtml = '';
+		for (let i = 0; i < users.length; i++) {
+			let user = users[i];
+			connectedUsersHtml += getConnectedUserHtml(user.publicId, user.username, user.colorGradientCss);
+		}
+		$connectedUsers.append(connectedUsersHtml);
+		// updateConnectedUsers();
+	});
+
+	//Registering
 	// $userForm.submit(function(e){
 	// 	e.preventDefault();
-	// 	socket.emit('new user', $username.val(), function(data){
+	// 	socket.emit('new_user', $username.val(), function(data){
 	// 		if(data){
 	// 			$userFormArea.hide();
 	// 			$messageArea.show();
@@ -33,11 +151,27 @@ $(function(){
 	// 	$username.val('');
 	// });
 
-	socket.on('get users', function(data){
-		var html = '';
-		for(i = 0; i < data.length; i++){
-			html += '<li class="list-group-item">' + data[i] +'</li>';
-		}
-		$users.html(html);
-	})
+	// socket.on('get users', function (data) {
+	// 	var html = '';
+	// 	for (i = 0; i < data.length; i++) {
+	// 		html += '<li class="list-group-item">' + data[i] + '</li>';
+	// 	}
+	// 	$users.html(html);
+	// })
+});
+
+// InputBox focus on chat click (if no text was selected)
+$('.chat_box').mouseup(function () {
+	var highlightedText = "";
+	if (window.getSelection) {
+		highlightedText = window.getSelection().toString();
+	}
+	else if (document.selection && document.selection.type != "Control") {
+		highlightedText = document.selection.createRange().text;
+	}
+	if (highlightedText != "") {
+		console.log("highlighted text");
+	} else {
+		$inputBox.focus();
+	}
 });
--- server.js
+++ server.js
@@ -2,9 +2,12 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var fs = require('fs');
 
-users = [];
-connections = [];
+//seems like __dirname is equal to including a . before name
+//i.e. __dirname + '/index.html' == './index.html'
+
+console.log('after calling readFile');
 
 server.listen(process.env.PORT || 3000);
 console.log('Server running...');
@@ -15,34 +18,106 @@ app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
+var connections = [];
+var circle_colors = [];
+
+fs.readFile(__dirname + '/circle_colors.json', 'utf8', function (err, contents) {
+	// console.log(contents);
+	circle_colors = JSON.parse(contents);
+	console.log(circle_colors.length); //16
+	console.log(circle_colors[0].name); //skyline
+});
+
 io.sockets.on('connection', function (socket) {
+	//this approach doesnt work as duplicates may occur, when people come and go
+	// socket.publicId = connections.length - 1; // sending this id to clients
+
+	let id = generateId();
+	socket.publicId = id;
 	connections.push(socket);
+
 	console.log('Connected: %s sockets connected', connections.length);
 
 	//Disconnect
 	socket.on('disconnect', function (data) {
-		users.splice(users.indexOf(socket.username), 1);
-		updateUsernames();
+		if (!(typeof socket.publicId === 'undefined' || socket.publicId === null)) {
+			io.sockets.emit('sys_user_left', socket.publicId);
+		}
 		connections.splice(connections.indexOf(socket), 1);
 		console.log('Disconnected: %s sockets connected', connections.length);
 	});
 
 	//Send message
-	socket.on('send message', function(data){
-		io.sockets.emit('new message', {msg: data, user: socket.username});
+	socket.on('send_message', function (msg) {
+		socket.broadcast.emit('new_message', {
+			"msg": msg,
+			"publicId": socket.publicId
+		}
+		);
+		// io.sockets.emit('new_message', {msg: data, user: socket.});
 	});
 
-	//New User
-	socket.on('new user', function(data, callback){
-		callback(true);
-		socket.username = data;
-		users.push(socket.username);
-		updateUsernames();
+	//Registering new user
+	socket.on('new_user', function (data) {
+		socket.username = data.username;
+		// console.log(socket.username);
+		let colorsValues = circle_colors[data.color_schema].colors;
+		let rgbValues = '';
+		for (let i = 0; i < colorsValues.length; i++) {
+			rgbValues += hexToRgbString(colorsValues[i]);
+			if (!(i == colorsValues.length - 1)) {
+				rgbValues += ',';
+			}
+		}
+		let user_circle_background_css_string = `background: linear-gradient(` + data.color_schema_deg + `deg,` + rgbValues + `);`;
+		// console.log(user_circle_background_css_string);
+
+		// background: linear-gradient(130deg, rgb(255, 0, 153), rgb(73, 50, 64));
+
+		socket.colorGradientCss = user_circle_background_css_string;
+		// console.log(socket.colors);
+		socket.broadcast.emit('sys_user_joined', {
+			"publicId": socket.publicId,
+			"username": socket.username,
+			"colorGradientCss": socket.colorGradientCss
+		});
 	});
 
-	function updateUsernames(){
-		console.log(users);
-		io.sockets.emit('get users', users);
-	}
-
+	socket.on('get_users', function () {
+		let userlist = [];
+		connections.forEach(connection => {
+			userlist.push({
+				"publicId": connection.publicId,
+				"username": connection.username,
+				"colorGradientCss": connection.colorGradientCss
+			});
+		})
+		socket.emit('serve_users', userlist);
+	})
 });
+
+function hexToRgbString(hex) {
+	hex = hex.replace('#', '');
+	let r = parseInt(hex.substring(0, 2), 16);
+	let g = parseInt(hex.substring(2, 4), 16);
+	let b = parseInt(hex.substring(4, 6), 16);
+	return 'rgb(' + r + "," + g + "," + b + ')';
+}
+// console.log("hexToRgbConverson: " + hexToRgbString("#CC00CC")); // "rgb(204,0,204)"
+
+function generateId() {
+	let taken_ids = [];
+	let id = -1;
+	for (let i = 0; i < connections.length; i++) {
+		taken_ids.push(connections[i].publicId);
+	}
+	for (let i = 0; i < taken_ids.length; i++) {
+		if (!(taken_ids.includes(i))) {
+			id = i;
+		}
+	}
+	if (id == -1) {
+		id = taken_ids.length;
+	}
+	return id;
+}
